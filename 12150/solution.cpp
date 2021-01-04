193abd96a057f035956665f43f0f53745446e59e
Mateus Azis <mateusazis@gmail.com>
2021-01-04 09:02:05 -0800

    Add solution to problem 12150 - Pole Position.

---
+++ 12150/solution.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+static int cars[100000];
+
+int main(int argc, char **argv)
+{
+    int numberOfCars;
+    std::cin >> numberOfCars;
+
+    while (numberOfCars)
+    {
+        bool validGrid = true;
+
+        for (int i = 0; i < numberOfCars; i++)
+        {
+            cars[i] = 0;
+        }
+
+        for (int i = 0; i < numberOfCars; i++)
+        {
+            int car, positionDiff;
+            std::cin >> car >> positionDiff;
+
+            int newPos = i + positionDiff;
+            if (newPos < 0 || newPos >= numberOfCars) {
+                validGrid = false;
+                continue;
+            }
+
+            if (cars[i + positionDiff])
+            {
+                validGrid = false;
+            }
+
+            cars[i + positionDiff] = car;
+        }
+
+        if (validGrid)
+        {
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                if (i > 0) {
+                    std::cout << " ";
+                }
+                std::cout << cars[i];
+            }
+            std::cout << std::endl;
+        }
+        else
+        {
+            std::cout << -1 << std::endl;
+        }
+
+        std::cin >> numberOfCars;
+    }
+    return 0;
+}
